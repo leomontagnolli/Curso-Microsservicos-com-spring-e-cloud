498aa168f097b73d4e0648aeecb12f246ecdde09
Leonardo <leomontagnolli@gmail.com>
2020-11-01 17:18:33 -0300

    Criando random port e eureka como balanceador e servidor unico

--- rh-worker/src/main/java/com/leo/rhworker/RhWorkerApplication.java
+++ rh-worker/src/main/java/com/leo/rhworker/RhWorkerApplication.java
@@ -2,8 +2,10 @@ package com.leo.rhworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RhWorkerApplication {
 
 	public static void main(String[] args) {
