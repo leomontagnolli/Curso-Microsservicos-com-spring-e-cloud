3edc9a98c26ccf51e3eb77fa221c2e12ef68c471
Leonardo <leomontagnolli@gmail.com>
2020-11-01 18:46:07 -0300

    Adicionando api gateway e centralizando requisicoes

---
+++ hr-apigateway-zuul/src/main/java/com/leo/hrapigatewayzuul/HrApigatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package com.leo.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableZuulProxy
+public class HrApigatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApigatewayZuulApplication.class, args);
+	}
+
+}
